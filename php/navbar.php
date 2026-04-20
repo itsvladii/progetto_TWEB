35bf18f076aa7433d53522eab8b80f0a2fe973c6
Vladut <76739754+itsvladii@users.noreply.github.com>
2026-04-20 16:23:04 +0200

    navbar con bootstrap

--- php/navbar.php
+++ php/navbar.php
@@ -1,17 +1,16 @@
 <link rel="stylesheet" href="css/navbar.css">
 <header>
-    <nav>
-
-        <div id="logo">
-            <a href='#'><h1><span class="ticket">Ticket</span><span class="two">Two</span></h1></a>
-        </div>
-
-        <div id="auth-buttons">
-            <ul id="nav-links">
-                <a href="#">Home</a>
-                <a href="#">EVENTI</a>
-                <button id="log-in">Log In</button>
-            </ul>
+    <nav class="navbar fixed-top">
+        <div class="container-fluid">
+            <div id="logo">
+                <h1><span class="ticket">Ticket</span><span class="two">Two</span></h1>
+            </div>
+            <div id="auth-buttons">
+                <ul id="nav-links">
+                    <a href="#">EVENTI</a>
+                    <button id="log-in">Log In</button>
+                </ul>
+            </div>
         </div>
     </nav>
 </header>
