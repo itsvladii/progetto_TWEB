4bdaf2fca91d5463ec1fa6137126c7f38f19c4f5
LAPTOP-SEQS3LC5\mauro mandolini <st10136@iismarconipieralisi.it>
2026-04-23 13:36:50 +0200

    icona sito

--- head.php
+++ head.php
@@ -9,6 +9,6 @@
 <!--import delle librerie CSS bootstrap -->
 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-sRIl4kxILFvY47J16cr9ZwB07vP4J8+LH7qKQnuqkuIAvNWLzeN8tE5YBujZqJLB" crossorigin="anonymous">
 <!-- icona -->
-<link rel="icon" href="img/logo/tickettwo_icon.png" type="image/icon type">
+<link rel="icon" href="img/logo/tickettwo_icon_sito.png" type="image/icon type">
 <!-- FontAwesome for icons -->
 <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css">
